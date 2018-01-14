dcc9012b11db611cf3abc3706e071e6a86f7a26a
Gashaw1 <akalc2004@yahoo.com>
2018-01-13 20:03:17 -0500

    .

---
+++ BooksSearch/BookModelsLayer/Models/AccessInfo.cs
@@ -0,0 +1,25 @@
+﻿namespace BookModelsLayer.Models
+{
+    public class AccessInfo
+    {
+        public string country { get; set; }
+        public string ViewAbility { get; set; }
+        public bool emeddable { get; set; }
+        public bool publicDomain { get; set; }
+        public string textToSpeechPermission { get; set; }
+        public Epub epub { get; set; }
+        public Pdf pdf { get; set; }
+        public string ReaderLink { get; set; }
+        public string AccessViewStatus { get; set; }
+        public bool quteSharingAllowed { get; set; }
+       
+    }
+
+    public class Pdf
+    {
+    }
+
+    public class Epub
+    {
+    }
+}
